40f388fb823b7f49e0b8fad4e46cc8533979356f
Nathan Michael <nmichael@cmu.edu>
2016-03-20 17:41:03 -0400

    Conversion to NED and fixing id bugs

--- mavros_extras/src/plugins/mocap_command.cpp
+++ mavros_extras/src/plugins/mocap_command.cpp
@@ -1,5 +1,6 @@
 #include <mavros/mavros_plugin.h>
 #include <pluginlib/class_list_macros.h>
+#include <eigen_conversions/eigen_msg.h>
 #include <quadrotor_msgs/RPMCommand.h>
 #include <quadrotor_msgs/CascadedCommand.h>
 #include <std_msgs/Bool.h>
@@ -56,7 +57,6 @@ namespace mavplugin {
 
     void sendRPMCommand(const quadrotor_msgs::RPMCommand& msg)
     {
-      puts("sendRPMCommand");
       // cmd type = 1 (rpm)
       sendMavlinkCommand(msg.motor_rpm[0], msg.motor_rpm[1],
                          msg.motor_rpm[2], msg.motor_rpm[3], 1, 0);
@@ -64,29 +64,47 @@ namespace mavplugin {
 
     void sendCascadedCommand(const quadrotor_msgs::CascadedCommand& msg)
     {
-      puts("sendCascadedCommand");
       // cmd type = 2 (cascaded)
       // input types:
       // 0: thrust setpoint
       // 1: att (quat) setpoint
       // 2: angular velocities
-      // 3: pgains
-      // 4: dgains
+      // 3: angular accelerations
+      // 4: pgains
+      // 5: dgains
 
-      sendMavlinkCommand(msg.thrust, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
-      sendMavlinkCommand(msg.orientation.w, msg.orientation.x,
-                         msg.orientation.y, msg.orientation.z, 2.0f, 1.0f);
-      sendMavlinkCommand(msg.angular_velocity.x, msg.angular_velocity.y,
-                         msg.angular_velocity.z, 0.0f, 2.0f, 2.0f);
-      sendMavlinkCommand(msg.kR.x, msg.kR.y, msg.kR.z, 0.0f, 2.0f, 3.0f);
-      sendMavlinkCommand(msg.kOm.x, msg.kOm.y, msg.kOm.z, 0.0f, 2.0f, 4.0f);
+      Eigen::Vector3d thrust =
+        UAS::transform_frame_enu_ned(Eigen::Vector3d(0.0, 0.0, msg.thrust));
+
+      Eigen::Quaterniond q_enu;
+      float q[4];
+
+      tf::quaternionMsgToEigen(msg.orientation, q_enu);
+      UAS::quaternion_to_mavlink(UAS::transform_frame_enu_ned(q_enu), q);
+
+      Eigen::Vector3d ang_vel =
+        UAS::transform_frame_enu_ned(Eigen::Vector3d(msg.angular_velocity.x,
+                                                     msg.angular_velocity.y,
+                                                     msg.angular_velocity.z));
+
+      Eigen::Vector3d ang_acc =
+        UAS::transform_frame_enu_ned(Eigen::Vector3d(msg.angular_acceleration.x,
+                                                     msg.angular_acceleration.y,
+                                                     msg.angular_acceleration.z));
+
+      sendMavlinkCommand(thrust.z(), 0.0f, 0.0f, 0.0f, 2.0f, 0.0f);
+      sendMavlinkCommand(q[0], q[1], q[2], q[3], 2.0f, 1.0f);
+      sendMavlinkCommand(ang_vel.x(), ang_vel.y(), ang_vel.z(), 0.0f, 2.0f, 2.0f);
+      sendMavlinkCommand(ang_acc.x(), ang_acc.y(), ang_acc.z(), 0.0f, 2.0f, 3.0f);
+      sendMavlinkCommand(msg.kR.x, msg.kR.y, msg.kR.z, 0.0f, 2.0f, 4.0f);
+      sendMavlinkCommand(msg.kOm.x, msg.kOm.y, msg.kOm.z, 0.0f, 2.0f, 5.0f);
     }
 
     void sendMavlinkCommand(float p1, float p2, float p3, float p4, float p5, float p6)
     {
       mavlink_command_long_t cmd;
-      cmd.target_system = UAS_FCU(uas)->get_system_id();
-      cmd.target_component = UAS_FCU(uas)->get_component_id();
+      cmd.target_system = uas->get_tgt_system();
+      cmd.target_component = uas->get_tgt_component();
       cmd.param1 = p1;
       cmd.param2 = p2;
       cmd.param3 = p3;
